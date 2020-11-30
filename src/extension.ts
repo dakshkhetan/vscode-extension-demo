d7cf6ac3b549fdef15df38a1c586960e4a4024a5
Daksh Khetan <2012daksh@gmail.com>
2020-11-30 18:07:11 +0530

    find-synonyms-extension-example

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fetch from 'node-fetch';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,11 +17,41 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'vscode-extension-demo.helloWorld',
-    () => {
+    async () => {
       // The code you place here will be executed every time your command is executed
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from synonym-finder!');
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        // Display a message box to the user
+        vscode.window.showInformationMessage(
+          'Hello World from synonym-finder!'
+        );
+        return;
+      }
+
+      const text = editor.document.getText(editor.selection);
+
+      const response = await fetch(
+        `https://api.datamuse.com/words?ml=${text.replace(' ', '+')}`
+      );
+      const data = await response.json();
+
+      const quickPick = vscode.window.createQuickPick();
+
+      quickPick.items = data.map((x: any) => ({ label: x.word }));
+      quickPick.onDidChangeSelection(([item]) => {
+        if (item) {
+          // vscode.window.showInformationMessage(item.label);
+          editor.edit((edit) => {
+            edit.replace(editor.selection, item.label);
+          });
+
+          quickPick.dispose();
+        }
+      });
+
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.show();
     }
   );
 
